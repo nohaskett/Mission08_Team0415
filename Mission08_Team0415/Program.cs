b523396bdef34bc010d0744c8fd70db8694ae23e
RedLynx101 <NoahHicks101@gmail.com>
2024-03-01 14:08:10 -0700

    All app settings are the same on master branch now

--- Mission08_Team0415/Program.cs
+++ Mission08_Team0415/Program.cs
@@ -6,10 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<AddTaskContext>(options =>
-{
-    options.UseSqlite(builder.Configuration["ConnectionStrings:TaskConnection"]);
-});
+// Register the DbContext here, before calling builder.Build()
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
